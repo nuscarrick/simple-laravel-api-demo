de56f89a4f199d75235e4bd3ed397a1ce13cc780
nuscarrick <carrick@nustechnology.com>
2024-09-10 13:42:59 +0700

    Add Submission model

---
+++ database/factories/SubmissionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Submission>
+ */
+class SubmissionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->text(20),
+            'email' => fake()->safeEmail(),
+            'message' => fake()->text(100)
+        ];
+    }
+}
