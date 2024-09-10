0fa3ca16909bed1b6e1b80f0b37f3098ba59072a
nuscarrick <carrick@nustechnology.com>
2024-09-10 14:04:04 +0700

    Add Laravel queue job to process save input data from HTTP request

--- app/Http/Controllers/Api/SubmissionsController.php
+++ app/Http/Controllers/Api/SubmissionsController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreSubmissionRequest;
+use  App\Jobs\ProcessStoreSubmission;
 
 class SubmissionsController extends Controller
 {   
     public function store(StoreSubmissionRequest $request)
     {
+        $data = $request->only('name', 'email', 'message');
+        
+        ProcessStoreSubmission::dispatch($data);        
+
         return response()->json(array('success' => true));
     }    
 }
